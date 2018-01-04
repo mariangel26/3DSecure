0e7b6ea8890a3aefac03373df6c9876c06d73ec5
oswallopezm <oswaldo7365@hotmail.com>
2018-01-04 02:53:28 -0400

    hasheando datos de tarjeta

--- PruebaBancoCliente/src/pruebabancocliente/PruebaBancoCliente.java
+++ PruebaBancoCliente/src/pruebabancocliente/PruebaBancoCliente.java
@@ -23,10 +23,24 @@ public class PruebaBancoCliente {
     public static void main(String[] args) throws IOException {
         new HiloPrincipalServidor().start();
         //SI SE NECESITA CREAR UNA CUENTA BANCARIA SE USA LO SIGUIENTE
-        //DAOCliente dao = new DAOCliente();
-        //Cliente cliente = new Cliente("oswaldo","lopez",25253393,"oswaldo7365@hotmail.com",4532314510308244L,400000L,"10","21",218);
-        //dao.registrarCliente(cliente);
-        //HiloProcesaServidor.enviarABancoVendedor("25253393;4532314510308244;10;21;218;oswaldo;lopez;350000");
+        /*String numeroCuenta = "4532314510308244";
+        Long cuentahash = new Long(PruebaBancoCliente.toHash(numeroCuenta));
+        String mes = "10";
+        String meshash = PruebaBancoCliente.toHash(mes).toString();
+        String ano = "2021";
+        String anohash = PruebaBancoCliente.toHash(ano).toString();
+        String numSeguridad = "218";
+        Integer numSeguridadhash = PruebaBancoCliente.toHash(numSeguridad);
+        DAOCliente dao = new DAOCliente();
+        Cliente cliente = new Cliente("oswaldo","lopez",25253393,"oswaldo7365@hotmail.com",cuentahash,400000L,meshash,anohash,numSeguridadhash);
+        dao.registrarCliente(cliente);
+        *///HiloProcesaServidor.enviarABancoVendedor("25253393;4532314510308244;10;21;218;oswaldo;lopez;350000");
+    }
+    
+    private static Integer toHash(String clave){
+        Integer hash = 512;
+        hash =  37*hash + clave.hashCode();
+        return hash;
     }
     
 }
